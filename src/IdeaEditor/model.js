12b3c5b75ab5208fa4f2f46e1379cee99dac1f81
yuxin13 <yuxin13@baidu.com>
2018-05-22 12:41:34 +0800

    1805recruit说明

---
+++ src/IdeaEditor/model.js
@@ -0,0 +1,57 @@
+/**
+ * Created by baidu on 17/4/14.
+ */
+const importTitleDialog = {
+    isOpen: false,
+    listData: [],
+    searchQuery: '',
+    isRequesting: true
+};
+
+const formData = {
+    title: '',
+    url: ''
+};
+
+const initialState = {
+    formData,
+    importTitleDialog
+};
+
+export default {
+    namespace: 'ideaEditor',
+    state: initialState,
+    reducers: {
+        changeFormField(state, {payload: {field, value}}) {
+            return {
+                ...state,
+                formData: {
+                    ...state.formData,
+                    [field]: value
+                }
+
+            };
+        },
+        resetFormData(state) {
+            return {
+                ...state,
+                formData
+            };
+        },
+        changeImportTitleDialog(state, {payload: importTitleDialog}) {
+            return {
+                ...state,
+                importTitleDialog: {
+                    ...state.importTitleDialog,
+                    ...importTitleDialog
+                }
+            };
+        },
+        resetImportTitleDialog(state) {
+            return {
+                ...state,
+                importTitleDialog
+            };
+        }
+    }
+};
---
+++ src/common/ui/FormWithValidation.jsx.js
@@ -0,0 +1,55 @@
+/**
+ * @file 表单验证公共组件
+ * @author chenmo04@baidu.com
+ * @date 2017-07-07
+ */
+import React from 'react';
+import _  from 'lodash';
+import {Form} from 'fcui2';
+
+class FormWithValidation extends React.Component {
+    constructor(...args) {
+        super(...args);
+
+        this.onFieldChange = this.onFieldChange.bind(this);
+    }
+    onFieldChange(form) {
+        this.props.onFieldChange(form);
+        const validationResults = form.validationResults;
+        const fieldErrors = {};
+        const errorMessageList = [];
+        const separator = this.props.separator;
+        _.forEach(validationResults, (val, name) => {
+            if (!val.length) {
+                return;
+            }
+            fieldErrors[name] = val[0];
+            val.map(item => errorMessageList.push(name + separator + item));
+        });
+        const errorMessage = errorMessageList.length ? errorMessageList : null;
+        if (form.field) {
+            this.props.onFieldValidate(fieldErrors, form);
+        }
+        else {
+            this.props.onSubmitValidate(errorMessage, validationResults, form);
+        }
+    }
+    render() {
+        const props = this.props;
+        const formProps = {
+            ...props,
+            onFieldChange: this.onFieldChange,
+            onSubmit: props.onSubmit
+        };
+        return (<Form {...formProps}>{props.children}</Form>);
+    }
+}
+
+FormWithValidation.defaultProps = {
+    separator: '：',
+    onFieldValidate: _.noop,
+    onSubmitValidate: _.noop,
+    onFieldChange: _.noop
+};
+
+export default FormWithValidation;
